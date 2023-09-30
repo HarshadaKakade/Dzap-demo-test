87ccf3854728e46e1ccbf15127b3608a5aeb621e
HarshadaKakade <72975898+HarshadaKakade@users.noreply.github.com>
2023-09-30 18:08:15 +0530

    Update index.tsx

--- src/components/ApplicationInputField/index.tsx
+++ src/components/ApplicationInputField/index.tsx
@@ -1,17 +1,17 @@
 import { ChangeEvent, KeyboardEvent } from 'react';
-const ApplicationInputFiled = ({ value, onChnageInputField }: { value: string, onChnageInputField: (arg: string) => void}) => {
+const ApplicationInputFiled = ({ value, onChnageInputField }: { value: string, onChnageInputField: (arg: string, isNewAddress?:boolean) => void}) => {
    const onChnageInputValue = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const value = e.target.value
     onChnageInputField(value)
    }
    const handleKeyDown = (e: KeyboardEvent<HTMLElement>) => {
     if(e.key === "Enter"){
-        onChnageInputField(value)
+        onChnageInputField(value, true)
     }
    }
     return (
         <div>
-            <textarea className='w-screen bg-transparent outline-black text-white border-transparent focus:outline-none' value={value} onKeyDown={handleKeyDown} onChange={(e) => onChnageInputValue(e)} />
+            <textarea className='w-screen h-96 bg-transparent outline-black text-white border-transparent focus:outline-none' value={value} onKeyDown={handleKeyDown} onChange={(e) => onChnageInputValue(e)} />
         </div>
     )
 }
